f2977e93c91e54b0e426ac8e8e9ad0a9fcc41ffd
Kevin Evans <kevans@thriftbooks.com>
2016-12-29 14:58:07 -0800

    use unsigned bitshift op

--- hash.js
+++ hash.js
@@ -14,17 +14,17 @@ function hash(filename) {
         esi = ((s[i + 11] << 24) | (s[i + 10] << 16) | (s[i + 9] << 8) | s[i + 8]) & MAX_UINT + esi;
         edx = ((s[i + 3] << 24) | (s[i + 2] << 16) | (s[i + 1] << 8) | s[i]) & MAX_UINT - esi;
 
-        edx = (edx + ebx) ^ (esi >> 28) ^ (esi << 4);
+        edx = (edx + ebx) ^ (esi >>> 28) ^ (esi << 4);
         esi += edi;
-        edi = (edi - edx) ^ (edx >> 26) ^ (edx << 6);
+        edi = (edi - edx) ^ (edx >>> 26) ^ (edx << 6);
         edx += esi;
-        esi = (esi - edi) ^ (edi >> 24) ^ (edi << 8);
+        esi = (esi - edi) ^ (edi >>> 24) ^ (edi << 8);
         edi += edx;
-        ebx = (edx - esi) ^ (esi >> 16) ^ (esi << 16);
+        ebx = (edx - esi) ^ (esi >>> 16) ^ (esi << 16);
         esi += edi;
-        edi = (edi - ebx) ^ (ebx >> 13) ^ (ebx << 19);
+        edi = (edi - ebx) ^ (ebx >>> 13) ^ (ebx << 19);
         ebx += esi;
-        esi = (esi - edi) ^ (edi >> 28) ^ (edi << 4);
+        esi = (esi - edi) ^ (edi >>> 28) ^ (edi << 4);
         edi += ebx;
     }
 
@@ -61,13 +61,13 @@ function hash(filename) {
                 break;
         }
 
-        esi = (esi ^ edi) - ((edi >> 18) ^ (edi << 14));
-        ecx = (esi ^ ebx) - ((esi >> 21) ^ (esi << 11));
-        edi = (edi ^ ecx) - ((ecx >> 7) ^ (ecx << 25));
-        esi = (esi ^ edi) - ((edi >> 16) ^ (edi << 16));
-        edx = (esi ^ ecx) - ((esi >> 28) ^ (esi << 4));
-        edi = (edi ^ edx) - ((edx >> 18) ^ (edx << 14));
-        eax = (esi ^ edi) - ((edi >> 8) ^ (edi << 24));
+        esi = (esi ^ edi) - ((edi >>> 18) ^ (edi << 14));
+        ecx = (esi ^ ebx) - ((esi >>> 21) ^ (esi << 11));
+        edi = (edi ^ ecx) - ((ecx >>> 7) ^ (ecx << 25));
+        esi = (esi ^ edi) - ((edi >>> 16) ^ (edi << 16));
+        edx = (esi ^ ecx) - ((esi >>> 28) ^ (esi << 4));
+        edi = (edi ^ edx) - ((edx >>> 18) ^ (edx << 14));
+        eax = (esi ^ edi) - ((edi >>> 8) ^ (edi << 24));
 
         return ((edi << 32) & MAX_UINT) | eax;
     }
