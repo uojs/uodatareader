556407dd7dc9c9c39e956405973d1d3bdd11d6f5
Kevin Evans <kevans@thriftbooks.com>
2017-01-04 14:57:04 -0800

    added better comments

--- test.js
+++ test.js
@@ -1,8 +1,19 @@
 const uodatareader = require('./index');
+
+// load map block:
+const felucca = new uodatareader.Map({
+    baseDirectory: '../tmp/uo',
+    mapId: 0
+});
+
+const area = felucca.getLandBlock(1, 1);
+console.log(area);
+
+// load land image as sharp image:
 const art = new uodatareader.Art({
     baseDirectory: '../tmp/uo'
 });
-
+// image is type 'sharp' (https://github.com/lovell/sharp)
 const image = art.loadLand(2);
 
 image.toFile('test.png');
