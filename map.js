f73b51f4592920fc6d69ae0daa8bcf8be6fd3304
lamo2k123 <lamo2k123@gmail.com>
2017-01-11 23:50:52 +0300

    fixed chunkHeight

--- map.js
+++ map.js
@@ -5,6 +5,7 @@ const FileIndexReader = require('./fileindexreader');
 const pad = require('./pad');
 
 class Map {
+
     constructor(options) {
         this.options = options;
         this.index = new FileIndexReader({
@@ -13,6 +14,18 @@ class Map {
             mulFile: `map${options.mapId}.mul`,
             uopFileExtension: 'dat'
         });
+
+        this.chunkHeightList = [
+            512,
+            512,
+            200,
+            256,
+            181
+        ];
+    }
+
+    get chunkHeight() {
+        return this.chunkHeightList[this.options.mapId];
     }
 
     getLandBlock(x, y) {
@@ -23,9 +36,60 @@ class Map {
         return this.readLandBlock(x, y);
     }
 
+    readMyMethod(x, y) {
+        /*
+         SizeOfLandChunk = 196
+
+         Block:
+            Y: real position (Y / 8)
+            X: real position (X / 8)
+            SIZE-CHUNK : 196
+            HEIGHT-CHUNK : map id: 0 = 512, 1 = 512, 2 = 200, 3 = 256, 4 = 181
+
+            (({X} * {HEIGHT-CHUNK}) + {Y}) * {SIZE-CHUNK} + 4
+
+         Cell:
+            Y: min `0` max `7`
+            X: min `0` max `7`
+
+            (({Y} * 8) + {X}) * 3
+
+         Block + Cell:
+            ((({X} * {HEIGHT-CHUNK}) + {Y}) * {SIZE-CHUNK} + 4) + (({Y} * 8) + {X}) * 3
+        */
+
+        // let offset = (((~~(x / 8) * this.chunkHeight) + ~~(y / 8)) * 196 + 4) + ((8 * 8) + 7) * 3;
+        let offset = ((~~(x / 8) * this.chunkHeight) + ~~(y / 8)) * 196 + 4;
+
+        console.log(1, offset);
+        if (this.index.isUOP) {
+            offset = this.calculateOffset(offset);
+        }
+
+        console.log(2, offset);
+        if (!this.index.reader.seek(offset)) {
+            throw `could not seek to ${offset}`;
+        }
+
+        console.log(3, offset);
+        /*console.log({
+            id : this.index.reader.nextUShort(),
+            z : this.index.reader.nextSByte()
+        });*/
+        return Array(64).fill(null).map((x, index) => {
+            const id = this.index.reader.nextUShort();
+            const z = this.index.reader.nextSByte();
+
+            return {
+                id,
+                z
+            };
+        });
+
+    }
+
     readLandBlock(x, y) {
-        const blockHeight = this.height >> 3;
-        let offset = ((x * blockHeight) + y) * 196 + 4;
+        let offset = ((x * this.chunkHeight) + y) * 196 + 4;
 
         if (this.index.isUOP) {
             offset = this.calculateOffset(offset);
